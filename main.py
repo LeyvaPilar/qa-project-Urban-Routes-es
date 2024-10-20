1565bc5367c1c78eeec52ae42580e8393c1b3d08
Pilar Leyva <pilar.leyva66@gmail.com>
2024-10-20 13:28:28 +0200

    Refactor usando el archivo methods.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from telnetlib import XAUTH
-
 import json
 import time
 import data
-import locators
+import methods
 
 from selenium.common import WebDriverException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
@@ -40,50 +35,6 @@ def retrieve_phone_code(driver) -> str:
                             "Utiliza 'retrieve_phone_code' solo después de haber solicitado el código en tu aplicación.")
         return code
 
-class UrbanRoutesPage:
-
-    def __init__(self, driver):
-        self.driver = driver
-        self.locators = locators.UrbanRoutesPage()
-
-    def set_from(self, from_address):
-        self.driver.find_element(*self.locators.from_field).send_keys(from_address)
-
-    def set_to(self, to_address):
-        self.driver.find_element(*self.locators.to_field).send_keys(to_address)
-
-    def get_from(self):
-        return self.driver.find_element(*self.locators.from_field).get_property('value')
-
-    def get_to(self):
-        return self.driver.find_element(*self.locators.to_field).get_property('value')
-
-    def set_route(self, address_from, address_to):
-        self.driver.find_element(*self.locators.from_field).send_keys(address_from)
-        self.driver.find_element(*self.locators.to_field).send_keys(address_to)
-
-    def wait_element_is_visible(self, locator):
-        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(locator))
-
-    def click_element(self, locator):
-        self.wait_element_is_visible(locator)
-        self.driver.find_element(*locator).click()
-
-    def fill_input(self, locator, text):
-        self.driver.find_element(*locator).send_keys(text)
-
-    def press_tab(self, locator):
-        self.driver.find_element(*locator).send_keys(Keys.TAB)
-
-    def scroll_to_element(self, locator):
-        element = self.driver.find_element(*locator)
-        self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'end'});", element)
-        time.sleep(1)
-
-    def click_checkbox_manta_panuelos(self):
-        self.driver.execute_script("document.querySelector('" + self.locators.checkbox_manta_panuelos + "').click()")
-
-
 class TestUrbanRoutes:
 
     driver = None
@@ -99,7 +50,7 @@ class TestUrbanRoutes:
 
     def fill_direction(self):
         self.driver.get(data.urban_routes_url)
-        routes_page = UrbanRoutesPage(self.driver)
+        routes_page = methods.UrbanRoutesPage(self.driver)
         # Wait element is visible
         routes_page.wait_element_is_visible(routes_page.locators.from_field)
         # Fill form, from & to
@@ -111,13 +62,13 @@ class TestUrbanRoutes:
         time.sleep(3)
         self.driver.save_screenshot("test_set_route_1.png")
 
-        routes_page = UrbanRoutesPage(self.driver)
+        routes_page = methods.UrbanRoutesPage(self.driver)
         assert routes_page.get_from() == data.address_from
         assert routes_page.get_to() == data.address_to
 
     def test_select_comfort_rate(self):
         self.fill_direction()
-        routes_page = UrbanRoutesPage(self.driver)
+        routes_page = methods.UrbanRoutesPage(self.driver)
         self.driver.save_screenshot("test_select_comfort_rate_1.png")
         routes_page.click_element(routes_page.locators.div_personal)
         routes_page.click_element(routes_page.locators.button_taxi)
@@ -159,8 +110,6 @@ class TestUrbanRoutes:
         time.sleep(35)
         self.driver.save_screenshot("test_select_comfort_rate_9.png")
 
-
     @classmethod
     def teardown_class(cls):
         cls.driver.quit()
-
---
+++ methods.py
@@ -0,0 +1,49 @@
+import locators
+import time
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
+
+class UrbanRoutesPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.locators = locators.UrbanRoutesPage()
+
+    def set_from(self, from_address):
+        self.driver.find_element(*self.locators.from_field).send_keys(from_address)
+
+    def set_to(self, to_address):
+        self.driver.find_element(*self.locators.to_field).send_keys(to_address)
+
+    def get_from(self):
+        return self.driver.find_element(*self.locators.from_field).get_property('value')
+
+    def get_to(self):
+        return self.driver.find_element(*self.locators.to_field).get_property('value')
+
+    def set_route(self, address_from, address_to):
+        self.driver.find_element(*self.locators.from_field).send_keys(address_from)
+        self.driver.find_element(*self.locators.to_field).send_keys(address_to)
+
+    def wait_element_is_visible(self, locator):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(locator))
+
+    def click_element(self, locator):
+        self.wait_element_is_visible(locator)
+        self.driver.find_element(*locator).click()
+
+    def fill_input(self, locator, text):
+        self.driver.find_element(*locator).send_keys(text)
+
+    def press_tab(self, locator):
+        self.driver.find_element(*locator).send_keys(Keys.TAB)
+
+    def scroll_to_element(self, locator):
+        element = self.driver.find_element(*locator)
+        self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'end'});", element)
+        time.sleep(1)
+
+    def click_checkbox_manta_panuelos(self):
+        self.driver.execute_script("document.querySelector('" + self.locators.checkbox_manta_panuelos + "').click()")
